e2446b1e6185d65e44571c5fd2cfcba0e408824d
Lutor Ayangaor <lutor4148@gmail.com>
2020-11-08 16:48:46 +0100

    fix bug

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Naira} from 'react-naira'
+
 
 class Pricing extends React.Component {
   render() {
@@ -18,7 +18,7 @@ class Pricing extends React.Component {
                 <div className="col-lg-4">
                     <div className="text-center pricing-box hover-effect">
                         <h4 className="text-uppercase">Digital Marketing</h4>
-                        <Naira>20,000</Naira> 
+                        <h1>20,000 </h1>
                         <h6 className="text-uppercase text-muted">Billing Per Month</h6>
                         <div className="pricing-border"></div>
                         <div className="plan-features margin-t-30">
@@ -34,7 +34,7 @@ class Pricing extends React.Component {
                 <div className="col-lg-4">
                     <div className="text-center pricing-box bg-white hover-effect price-active">
                         <h4 className="text-uppercase">Web Develoment</h4>
-                        <Naira>45,000</Naira>
+                        <h1>45,000</h1>
                         <h6 className="text-uppercase text-muted">Billing Per Month</h6>
                         <div className="pricing-border"></div>
                         <div className="plan-features margin-t-30">
@@ -50,7 +50,7 @@ class Pricing extends React.Component {
                 <div className="col-lg-4">
                     <div className="text-center pricing-box hover-effect">
                         <h4 className="text-uppercase">Mobile Development</h4>
-                        <Naira>78,000</Naira>
+                        <h1>78,000</h1>
                         <h6 className="text-uppercase text-muted">Billing Per Month</h6>
                         <div className="pricing-border"></div>
                         <div className="plan-features margin-t-30">
