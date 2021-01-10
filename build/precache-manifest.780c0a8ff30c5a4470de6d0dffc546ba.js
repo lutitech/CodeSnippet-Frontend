343b19913614688a486a4804f22fa24f5d64c553
Lutor Ayangaor <lutor4148@gmail.com>
2021-01-10 10:09:48 +0100

    build

---
+++ build/precache-manifest.780c0a8ff30c5a4470de6d0dffc546ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cc5de2abb8465b44d95",
+    "url": "/static/js/main.6cc5de2a.chunk.js"
+  },
+  {
+    "revision": "b447847517ed304ea37c",
+    "url": "/static/js/1.b4478475.chunk.js"
+  },
+  {
+    "revision": "8f786ad7e5d6565eff347d812b097749",
+    "url": "/index.html"
+  }
+];
